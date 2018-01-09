923888d0d5fcc9f51de21d4fe5bde0c36d44ffa3
ilgulee <iglee2000ca@gmail.com>
2018-01-09 03:30:40 -0500

    Added 3 Flocks

--- Assets/AllUnits.cs
+++ Assets/AllUnits.cs
@@ -11,6 +11,13 @@ namespace Assets
 		public int NumberOfUnits = 10;
 
 		public Vector3 DefaultRange=new Vector3(5,5,5);
+	    public bool SeekGoal = true;
+	    public bool Obedient = true;
+	    public bool Willful = false;
+
+	    [Range(0, 200)] public int NeighbourDistance = 50;
+	    [Range(0, 2)] public float MaxForce = 0.5f;
+	    [Range(0, 5)] public float MaxVelocity = 2.0f;
 
 		void OnDrawGizmosSelected()
 		{
@@ -25,7 +32,7 @@ namespace Assets
 			Units=new GameObject[NumberOfUnits];
 			for (int i = 0; i < Units.Length; i++)
 			{
-				var unitPosition=new Vector3(Random.Range(-DefaultRange.x,DefaultRange.y),
+				var unitPosition=new Vector3(Random.Range(-DefaultRange.x,DefaultRange.x),
 												 Random.Range(-DefaultRange.y,DefaultRange.y),
 												 Random.Range(0,0));
 				Units[i] = Instantiate(BoidPrefab, this.transform.position + unitPosition, Quaternion.identity);
--- Assets/UnitBehavior.cs
+++ Assets/UnitBehavior.cs
@@ -1,4 +1,5 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 
 namespace Assets
 {
@@ -8,9 +9,9 @@ namespace Assets
 		public Vector2 Location=Vector2.zero;
 
 		public Vector2 Velocity;
-	    private Vector2 _goalPosition=Vector2.zero;
+		private Vector2 _goalPosition=Vector2.zero;
 
-	    private Vector2 _currentForce;
+		private Vector2 _currentForce;
 
 		// Use this for initialization
 		void Start () {
@@ -26,7 +27,18 @@ namespace Assets
 		void ApplyForce(Vector2 f)
 		{
 			Vector3 force=new Vector3(f.x,f.y,0);
+			if (force.magnitude > UnitManager.GetComponent<AllUnits>().MaxForce)
+			{
+				force = force.normalized;
+				force *= UnitManager.GetComponent<AllUnits>().MaxForce;
+			}
 			this.GetComponent<Rigidbody2D>().AddForce(force);
+
+			if (this.GetComponent<Rigidbody2D>().velocity.magnitude > UnitManager.GetComponent<AllUnits>().MaxVelocity)
+			{
+				this.GetComponent<Rigidbody2D>().velocity = this.GetComponent<Rigidbody2D>().velocity.normalized;
+				this.GetComponent<Rigidbody2D>().velocity *= UnitManager.GetComponent<AllUnits>().MaxVelocity;
+			}
 			Debug.DrawRay(this.transform.position,force,Color.blue);
 		}
 
@@ -34,16 +46,82 @@ namespace Assets
 		{
 			Location = this.transform.position;
 			Velocity = this.GetComponent<Rigidbody2D>().velocity;
-			var goal = Seek(_goalPosition);
-			_currentForce = goal;
-			_currentForce = _currentForce.normalized;
-
+			if (UnitManager.GetComponent<AllUnits>().Obedient && Random.Range(0, 50) <= 1)
+			{
+				Vector2 ali = Align();
+				Vector2 coh = Cohesion();
+				if (UnitManager.GetComponent<AllUnits>().SeekGoal)
+				{
+					var goal = Seek(_goalPosition);
+					_currentForce = goal + ali + coh;
+				}
+				else
+				{
+					_currentForce = ali + coh;
+				}
+				
+				_currentForce = _currentForce.normalized;
+			}
+			if (UnitManager.GetComponent<AllUnits>().Willful && Random.Range(0, 50) <= 1)
+			{
+				if (Random.Range(0, 50) < 1)
+				{
+					_currentForce=new Vector2(Random.Range(0.01f,0.1f),Random.Range(0.01f,0.1f));
+				}
+			}
 			ApplyForce(_currentForce);
 		}
 		// Update is called once per frame
 		void Update () {
 			Flock();
 			_goalPosition = UnitManager.transform.position;
+		}
+
+		Vector2 Align()
+		{
+			float neighbordist = UnitManager.GetComponent<AllUnits>().NeighbourDistance;
+			Vector2 sum = Vector2.zero;
+			int count = 0;
+			foreach (GameObject other in UnitManager.GetComponent<AllUnits>().Units)
+			{
+				if (other == this.gameObject) continue;
+				float d = Vector2.Distance(Location, other.GetComponent<UnitBehavior>().Location);
+				if (d < neighbordist)
+				{
+					sum += other.GetComponent<UnitBehavior>().Velocity;
+					count++;
+				}
+			}
+			if (count > 0)
+			{
+				sum /= count;
+				Vector2 steer = sum - Velocity;
+				return steer;
+			}
+			return Vector2.zero;
+		}
+
+		Vector2 Cohesion()
+		{
+			float neighbordist = UnitManager.GetComponent<AllUnits>().NeighbourDistance;
+			Vector2 sum = Vector2.zero;
+			int count = 0;
+			foreach (GameObject other in UnitManager.GetComponent<AllUnits>().Units)
+			{
+				if (other == this.gameObject) continue;
+				float d = Vector2.Distance(Location, other.GetComponent<UnitBehavior>().Location);
+				if (d < neighbordist)
+				{
+					sum += other.GetComponent<UnitBehavior>().Velocity;
+					count++;
+				}
+			}
+			if (count > 0)
+			{
+				sum /= count;
+				return Seek(sum);
+			}
+			return Vector2.zero;
 		}
 	}
 }
